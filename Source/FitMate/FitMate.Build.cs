ba06be4b8c3e198a1ab77efc6c020730417cebdc
ryi01 <kkjmo3@naver.com>
2024-08-22 01:21:23 +0900

    openlevel

--- Source/FitMate/FitMate.Build.cs
+++ Source/FitMate/FitMate.Build.cs
@@ -8,7 +8,7 @@ public class FitMate : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "HTTP", "Json", "JsonUtilities", "WebBrowser", "WebBrowserWidget",  "MediaAssets" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "HTTP", "Json", "JsonUtilities", "WebBrowser", "WebBrowserWidget",  "MediaAssets", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
